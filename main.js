f0ed36bb83a5abd447649da553255b3ec7d096d8
Strovsk (Thiago) <strovsk@outlook.com>
2023-04-02 19:37:08 -0300

    handle files

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-const calendar = new Calendar({ pos: { x: 500, y: 50 }, isRange: true, selectionType: 'range' });
+const calendar = new HemeraCalendar({
+    // pos: { x: 500, y: 50 },
+    container: '#openDateSelector',
+    static: false,
+    closeAfterSelect: true,
+    selectionType: '1',
+});
 console.log(calendar.containerElm);
 document.body.appendChild(calendar.containerElm);
+
+document.getElementById('openDateSelector').addEventListener('click', () => {
+    calendar.toggle();
+});
 // calendar.selectRange(10, 15);
