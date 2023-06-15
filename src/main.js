fe7f112bfc8015b64958009cddf9e6985e44cf35
Thiago Santa Clara <strovsk@outlook.com>
2023-06-14 23:52:42 -0300

    build: add dist

--- src/main.js
+++ src/main.js
@@ -1,32 +1,33 @@
 const calendar = new HemeraCalendar({
-    // pos: { x: 500, y: 50 },
+    // pos: { x: center, y: 50 },
     container: '#openDateSelector',
-    static: true,
+    stayOnTop: true,
     closeAfterSelect: false,
-    selectionType: '1',
+    // selectionType: '1',
     // onSelect: (selections) => {},
     // onConfirmation: (selctions) => {},
 });
+calendar.init();
 console.log(calendar.containerElm);
 // calendar.hideDatesArea();
-document.body.appendChild(calendar.containerElm);
+// document.body.appendChild(calendar.containerElm);
 
 document.getElementById('openDateSelector').addEventListener('click', () => {
     calendar.toggle();
 });
 // calendar.selectRange(10, 15);
 
-const selections = [
-    {
-        "year": 2023,
-        "month": 2,
-        "date": 26,
-        "isToday": false,
-        "isAnotherMonth": true,
-        "isLastMonth": true,
-        "isNextMonth": false,
-        "isSubSelected": false,
-        "isSelected": false,
-        "dateObj": new Date(2023, 2, 26)
-    },
-];
+// const selections = [
+//     {
+//         "year": 2023,
+//         "month": 2,
+//         "date": 26,
+//         "isToday": false,
+//         "isAnotherMonth": true,
+//         "isLastMonth": true,
+//         "isNextMonth": false,
+//         "isSubSelected": false,
+//         "isSelected": false,
+//         "dateObj": new Date(2023, 2, 26)
+//     },
+// ];
