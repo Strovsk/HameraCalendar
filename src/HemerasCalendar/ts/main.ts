9af66da166d31c3b0a417ac4414bb88cffe821ee
Thiago Santa Clara <strovsk@outlook.com>
2023-12-02 16:39:19 -0300

    chore: import css file

--- src/HemerasCalendar/ts/main.ts
+++ src/HemerasCalendar/ts/main.ts
@@ -1,5 +1,6 @@
-import { HemeraCalendar } from "./HemeraCalendar";
-import { HemeraCalendarEngine } from "./HemeraCalendarEngine";
+import { HemeraCalendar } from './HemeraCalendar';
+import { HemeraCalendarEngine } from './HemeraCalendarEngine';
+import '@css/style.css';
 
 window.HemeraCalendarEngine = HemeraCalendarEngine;
 window.HemeraCalendar = HemeraCalendar;
